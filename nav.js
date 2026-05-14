2034811354a1590f53fc480b54cb7cf328ff3024
Aspect13 <ad13box@gmail.com>
2026-05-14 22:04:59 +0200

    Refactor navigation structure by replacing names with IDs for menu items and cleaning up commented code

--- nav.js
+++ nav.js
@@ -1,23 +1,11 @@
-/**
-  * This file is protected by copyright (c) 2023-2025 by BlueSky Statistics, LLC.
-  * All rights reserved. The copy, modification, or distribution of this file is not
-  * allowed without the prior written permission from BlueSky Statistics, LLC.
- */
-
-const {getT} = global.requireFromRoot("localization");
-let t = getT('menutoolbar')
-const nav = () => ({
-    "name": t('modeltuning_top_level_title'),// {ns: 'menutoolbar'}),
-    "tab": "model_tuning",
+const nav = {
+    "id": "menu-modeltuning",
     "buttons": [
         "./bootstrapResampling",
         "./kFoldCrossValidation",
         "./leaveOneOutCrossValidation",
         "./repkFoldCrossValidation"
     ]
-})
-
-module.exports = {
-    nav: nav(),
-    render: () => nav()
 }
+
+module.exports.nav = nav
